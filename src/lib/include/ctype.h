9c8dacd4c64f2c02f4f651235eb46e03ad55fa91
pino <mizuno@helloidea.org>
2011-09-20 13:38:08 +0900

    大規模な変更
    newlib
    ・string.hの移動
    ・newlibヘッダインクルード書き換え
    
    Signed-off-by: pino <mizuno@helloidea.org>

--- src/lib/include/ctype.h
+++ src/lib/include/ctype.h
@@ -1,14 +1,36 @@
 /*
  * ctype.h
- *
- *  Created on: 2011/03/28
- *      Author: mizu
- */
+*/
 
-#ifndef CTYPE_H_
-#define CTYPE_H_
+#ifndef _CTYPE_H_
+#define _CTYPE_H_
 
-int tolower(int c);
-int toupper(int c);
+#define _U      01
+#define _L      02
+#define _N      04
+#define _S      010
+#define _P      020
+#define _C      040
+#define _X      0100
+#define _B      0200
 
-#endif /* CTYPE_H_ */
+
+int isalpha(int);
+int isupper(int);
+int islower(int);
+int isdigit(int);
+int isxdigit(int);
+int isspace(int);
+int ispunct(int);
+int isalnum(int);
+int isprint(int);
+int isgraph(int);
+int iscntrl(int);
+int toupper(int);
+int tolower(int);
+
+extern	const char	*__ctype_ptr;
+extern	const char	_ctype_[];  /* For backward compatibility.  */
+
+#endif /* _CTYPE_H_ */
+
--- src/lib/include/stdlib.h
+++ src/lib/include/stdlib.h
@@ -1,17 +1,60 @@
 /*
  * stdlib.h
- *
- *  Created on: 2011/03/28
- *      Author: mizu
- */
+*/
 
-#ifndef STDLIB_H_
-#define STDLIB_H_
+#ifndef _STDLIB_H_
+#define _STDLIB_H_
 
-#include "string.h"
+#include "stddef.h"     /*For size_t and wchar_t*/
 
-int atoi(const char* _str);
-int decto10(char* _str);
-void asciitobin(char* _str, size_t size);
+#ifndef NULL
+#define NULL    ((void*) 0 )
+#endif
 
-#endif /* STDLIB_H_ */
+typedef struct
+{
+  int   quot;
+  int   rem;
+} div_t;
+
+typedef struct
+{
+  long int      quot;
+  long int      rem;
+} ldiv_t;
+
+
+#define RAND_MAX        32767
+
+#define EXIT_SUCCESS    0
+#define EXIT_FAILURE    1
+
+#define MB_CUR_MAX      1
+
+
+void *malloc(size_t);
+void free(void *);
+void exit(int);
+void *calloc(size_t, size_t);
+void *realloc(void *, size_t);
+int  atoi(const char *);
+long atol(const char *);
+double atof(const char *);
+double strtod(const char *, char **);
+long int strtol(const char *, char **, int);
+unsigned long int strtoul(const char *, char **, int);
+int rand(void);
+void srand(unsigned int);
+void abort(void);
+int abs(int);
+div_t div(int, int);
+long int labs(long int);
+ldiv_t ldiv(long int, long int);
+void *bsearch(const void *, const void *,
+              size_t, size_t,
+              int (*)(const void *,const void *));
+void qsort(void *, size_t, size_t,
+           int (*)(const void *, const void *));
+
+#endif /* _STDLIB_H_ */
+
