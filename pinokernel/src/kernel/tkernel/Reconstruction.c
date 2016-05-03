091cec40f0262bd1a3ecb396dc4038e9a2697d0a
mk_pino <monyuonyu@helloidea.org>
2016-05-03 16:40:12 +0900

    porting-tkernel/kernel再構成(cとhのみ)porting-tkernel/kernel再構成

--- pinokernel/src/kernel/tkernel/Reconstruction.c
+++ pinokernel/src/kernel/tkernel/Reconstruction.c
@@ -1448,7 +1448,6 @@
 #define tk_evt_dev_impl tk_evt_dev
 #define tk_def_dev_impl tk_def_dev
 #define tk_ref_idv_impl tk_ref_idv
-#define _SVC_ENTRY(name)    .long   _name##_impl
 #define N_TFN   99
 #define NULL        0
 #define TA_NULL     0U
@@ -3205,8 +3204,7 @@ static const WSPEC knl_wspec_flg_tpri  = { TTW_FLG, flg_chg_pri, NULL };
 
 /*** bit operation ***/
 /* tstdlib_bitclr : clear specified bit */
-void
-knl_tstdlib_bitclr( void *base, W offset )
+void knl_tstdlib_bitclr( void *base, W offset )
 {
     register UB *cp, mask;
 
@@ -3223,8 +3221,7 @@ knl_tstdlib_bitclr( void *base, W offset )
 }
 
 /* tstdlib_bitset : set specified bit */
-void
-knl_tstdlib_bitset( void *base, W offset )
+void knl_tstdlib_bitset( void *base, W offset )
 {
     register UB *cp, mask;
 
@@ -3241,8 +3238,7 @@ knl_tstdlib_bitset( void *base, W offset )
 }
 
 /* tstdlib_bitsearch1 : perform 1 search on bit string */
-W
-knl_tstdlib_bitsearch1( void *base, W offset, W width )
+W knl_tstdlib_bitsearch1( void *base, W offset, W width )
 {
     register UB *cp, mask;
     register W position;
@@ -3528,8 +3524,7 @@ void knl_set_reg( TCB *tcb, const T_REGS *regs, const T_EIT *eit, const T_CREGS 
 /*
  * Set task register contents
  */
-ER tk_set_reg_impl( ID tskid,
-        const T_REGS *pk_regs, const T_EIT *pk_eit, const T_CREGS *pk_cregs )
+ER tk_set_reg_impl( ID tskid, const T_REGS *pk_regs, const T_EIT *pk_eit, const T_CREGS *pk_cregs )
 {
     TCB     *tcb;
     ER      ercd = E_OK;
@@ -13509,52 +13504,52 @@ INT tk_can_wup_impl( ID tskid )
 
 
 
-    _TDSC_ENTRY(td_lst_tsk)
-    _TDSC_ENTRY(td_lst_sem)
-    _TDSC_ENTRY(td_lst_flg)
-    _TDSC_ENTRY(td_lst_mbx)
-    _TDSC_ENTRY(td_lst_mtx)
-    _TDSC_ENTRY(td_lst_mbf)
-    _TDSC_ENTRY(td_lst_por)
-    _TDSC_ENTRY(td_lst_mpf)
-    _TDSC_ENTRY(td_lst_mpl)
-    _TDSC_ENTRY(td_lst_cyc)
-    _TDSC_ENTRY(td_lst_alm)
-    _TDSC_ENTRY(td_lst_ssy)
-    _TDSC_ENTRY(td_ref_sem)
-    _TDSC_ENTRY(td_ref_flg)
-    _TDSC_ENTRY(td_ref_mbx)
-    _TDSC_ENTRY(td_ref_mtx)
-    _TDSC_ENTRY(td_ref_mbf)
-    _TDSC_ENTRY(td_ref_por)
-    _TDSC_ENTRY(td_ref_mpf)
-    _TDSC_ENTRY(td_ref_mpl)
-    _TDSC_ENTRY(td_ref_cyc)
-    _TDSC_ENTRY(td_ref_alm)
-    _TDSC_ENTRY(td_ref_ssy)
-    _TDSC_ENTRY(td_ref_tsk)
-    _TDSC_ENTRY(td_inf_tsk)
-    _TDSC_ENTRY(td_get_reg)
-    _TDSC_ENTRY(td_set_reg)
-    _TDSC_ENTRY(td_ref_sys)
-    _TDSC_ENTRY(td_get_tim)
-    _TDSC_ENTRY(td_get_otm)
-    _TDSC_ENTRY(td_rdy_que)
-    _TDSC_ENTRY(td_sem_que)
-    _TDSC_ENTRY(td_flg_que)
-    _TDSC_ENTRY(td_mbx_que)
-    _TDSC_ENTRY(td_mtx_que)
-    _TDSC_ENTRY(td_smbf_que)
-    _TDSC_ENTRY(td_rmbf_que)
-    _TDSC_ENTRY(td_cal_que)
-    _TDSC_ENTRY(td_acp_que)
-    _TDSC_ENTRY(td_mpf_que)
-    _TDSC_ENTRY(td_mpl_que)
-    _TDSC_ENTRY(td_hok_svc)
-    _TDSC_ENTRY(td_hok_dsp)
-    _TDSC_ENTRY(td_hok_int)
-    _TDSC_ENTRY(td_ref_dsname)
-    _TDSC_ENTRY(td_set_dsname)
+//    _TDSC_ENTRY(td_lst_tsk)
+//    _TDSC_ENTRY(td_lst_sem)
+//    _TDSC_ENTRY(td_lst_flg)
+//    _TDSC_ENTRY(td_lst_mbx)
+//    _TDSC_ENTRY(td_lst_mtx)
+//    _TDSC_ENTRY(td_lst_mbf)
+//    _TDSC_ENTRY(td_lst_por)
+//    _TDSC_ENTRY(td_lst_mpf)
+//    _TDSC_ENTRY(td_lst_mpl)
+//    _TDSC_ENTRY(td_lst_cyc)
+//    _TDSC_ENTRY(td_lst_alm)
+//    _TDSC_ENTRY(td_lst_ssy)
+//    _TDSC_ENTRY(td_ref_sem)
+//    _TDSC_ENTRY(td_ref_flg)
+//    _TDSC_ENTRY(td_ref_mbx)
+//    _TDSC_ENTRY(td_ref_mtx)
+//    _TDSC_ENTRY(td_ref_mbf)
+//    _TDSC_ENTRY(td_ref_por)
+//    _TDSC_ENTRY(td_ref_mpf)
+//    _TDSC_ENTRY(td_ref_mpl)
+//    _TDSC_ENTRY(td_ref_cyc)
+//    _TDSC_ENTRY(td_ref_alm)
+//    _TDSC_ENTRY(td_ref_ssy)
+//    _TDSC_ENTRY(td_ref_tsk)
+//    _TDSC_ENTRY(td_inf_tsk)
+//    _TDSC_ENTRY(td_get_reg)
+//    _TDSC_ENTRY(td_set_reg)
+//    _TDSC_ENTRY(td_ref_sys)
+//    _TDSC_ENTRY(td_get_tim)
+//    _TDSC_ENTRY(td_get_otm)
+//    _TDSC_ENTRY(td_rdy_que)
+//    _TDSC_ENTRY(td_sem_que)
+//    _TDSC_ENTRY(td_flg_que)
+//    _TDSC_ENTRY(td_mbx_que)
+//    _TDSC_ENTRY(td_mtx_que)
+//    _TDSC_ENTRY(td_smbf_que)
+//    _TDSC_ENTRY(td_rmbf_que)
+//    _TDSC_ENTRY(td_cal_que)
+//    _TDSC_ENTRY(td_acp_que)
+//    _TDSC_ENTRY(td_mpf_que)
+//    _TDSC_ENTRY(td_mpl_que)
+//    _TDSC_ENTRY(td_hok_svc)
+//    _TDSC_ENTRY(td_hok_dsp)
+//    _TDSC_ENTRY(td_hok_int)
+//    _TDSC_ENTRY(td_ref_dsname)
+//    _TDSC_ENTRY(td_set_dsname)
 
 
 /*
@@ -14868,105 +14863,7 @@ void knl_t_kernel_exit( void )
 
 
 
-    _SVC_ENTRY(tk_cre_tsk)
-    _SVC_ENTRY(tk_del_tsk)
-    _SVC_ENTRY(tk_sta_tsk)
-    _SVC_ENTRY(tk_ext_tsk)
-    _SVC_ENTRY(tk_exd_tsk)
-    _SVC_ENTRY(tk_ter_tsk)
-    _SVC_ENTRY(tk_dis_dsp)
-    _SVC_ENTRY(tk_ena_dsp)
-    _SVC_ENTRY(tk_chg_pri)
-    _SVC_ENTRY(tk_rot_rdq)
-    _SVC_ENTRY(tk_rel_wai)
-    _SVC_ENTRY(tk_get_tid)
-    _SVC_ENTRY(tk_get_reg)
-    _SVC_ENTRY(tk_set_reg)
-    _SVC_ENTRY(tk_ref_tsk)
-    _SVC_ENTRY(tk_sus_tsk)
-    _SVC_ENTRY(tk_rsm_tsk)
-    _SVC_ENTRY(tk_frsm_tsk)
-    _SVC_ENTRY(tk_slp_tsk)
-    _SVC_ENTRY(tk_wup_tsk)
-    _SVC_ENTRY(tk_can_wup)
-    _SVC_ENTRY(tk_cre_sem)
-    _SVC_ENTRY(tk_del_sem)
-    _SVC_ENTRY(tk_sig_sem)
-    _SVC_ENTRY(tk_wai_sem)
-    _SVC_ENTRY(tk_ref_sem)
-    _SVC_ENTRY(tk_cre_mtx)
-    _SVC_ENTRY(tk_del_mtx)
-    _SVC_ENTRY(tk_loc_mtx)
-    _SVC_ENTRY(tk_unl_mtx)
-    _SVC_ENTRY(tk_ref_mtx)
-    _SVC_ENTRY(tk_cre_flg)
-    _SVC_ENTRY(tk_del_flg)
-    _SVC_ENTRY(tk_set_flg)
-    _SVC_ENTRY(tk_clr_flg)
-    _SVC_ENTRY(tk_wai_flg)
-    _SVC_ENTRY(tk_ref_flg)
-    _SVC_ENTRY(tk_cre_mbx)
-    _SVC_ENTRY(tk_del_mbx)
-    _SVC_ENTRY(tk_snd_mbx)
-    _SVC_ENTRY(tk_rcv_mbx)
-    _SVC_ENTRY(tk_ref_mbx)
-    _SVC_ENTRY(tk_cre_mbf)
-    _SVC_ENTRY(tk_del_mbf)
-    _SVC_ENTRY(tk_snd_mbf)
-    _SVC_ENTRY(tk_rcv_mbf)
-    _SVC_ENTRY(tk_ref_mbf)
-    _SVC_ENTRY(tk_cre_por)
-    _SVC_ENTRY(tk_del_por)
-    _SVC_ENTRY(tk_cal_por)
-    _SVC_ENTRY(tk_acp_por)
-    _SVC_ENTRY(tk_fwd_por)
-    _SVC_ENTRY(tk_rpl_rdv)
-    _SVC_ENTRY(tk_ref_por)
-    _SVC_ENTRY(tk_def_int)
-    _SVC_ENTRY(tk_ret_int)
-    _SVC_ENTRY(tk_cre_mpl)
-    _SVC_ENTRY(tk_del_mpl)
-    _SVC_ENTRY(tk_get_mpl)
-    _SVC_ENTRY(tk_rel_mpl)
-    _SVC_ENTRY(tk_ref_mpl)
-    _SVC_ENTRY(tk_cre_mpf)
-    _SVC_ENTRY(tk_del_mpf)
-    _SVC_ENTRY(tk_get_mpf)
-    _SVC_ENTRY(tk_rel_mpf)
-    _SVC_ENTRY(tk_ref_mpf)
-    _SVC_ENTRY(tk_set_tim)
-    _SVC_ENTRY(tk_get_tim)
-    _SVC_ENTRY(tk_get_otm)
-    _SVC_ENTRY(tk_dly_tsk)
-    _SVC_ENTRY(tk_cre_cyc)
-    _SVC_ENTRY(tk_del_cyc)
-    _SVC_ENTRY(tk_sta_cyc)
-    _SVC_ENTRY(tk_stp_cyc)
-    _SVC_ENTRY(tk_ref_cyc)
-    _SVC_ENTRY(tk_cre_alm)
-    _SVC_ENTRY(tk_del_alm)
-    _SVC_ENTRY(tk_sta_alm)
-    _SVC_ENTRY(tk_stp_alm)
-    _SVC_ENTRY(tk_ref_alm)
-    _SVC_ENTRY(tk_ref_ver)
-    _SVC_ENTRY(tk_ref_sys)
-    _SVC_ENTRY(tk_def_ssy)
-    _SVC_ENTRY(tk_ref_ssy)
-    _SVC_ENTRY(tk_opn_dev)
-    _SVC_ENTRY(tk_cls_dev)
-    _SVC_ENTRY(tk_rea_dev)
-    _SVC_ENTRY(tk_srea_dev)
-    _SVC_ENTRY(tk_wri_dev)
-    _SVC_ENTRY(tk_swri_dev)
-    _SVC_ENTRY(tk_wai_dev)
-    _SVC_ENTRY(tk_sus_dev)
-    _SVC_ENTRY(tk_get_dev)
-    _SVC_ENTRY(tk_ref_dev)
-    _SVC_ENTRY(tk_oref_dev)
-    _SVC_ENTRY(tk_lst_dev)
-    _SVC_ENTRY(tk_evt_dev)
-    _SVC_ENTRY(tk_def_dev)
-    _SVC_ENTRY(tk_ref_idv)
+
 
 
 
